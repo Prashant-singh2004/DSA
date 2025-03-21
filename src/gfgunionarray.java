ab8a9cecb437b51befb5cdf4e014de6a2bae3f52
Prashant-singh2004 <131868224+Prashant-singh2004@users.noreply.github.com>
2025-03-21 22:48:32 +0530

    Corrected code with tree set and within time complexity

--- src/gfgunionarray.java
+++ src/gfgunionarray.java
@@ -1,34 +1,24 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.*;
 
 public class gfgunionarray {
     public static void main(String[] args) {
         int[] a = {-8, -3, -3, -2, 0, 1, 2, 2, 6};
-        int[] b = {-9, -9, 0,
-        };
+        int[] b = {-9, -9, 0,};
 
         System.out.println(findUnion(a,b));
     }
-    public static  ArrayList<Integer> findUnion(int a[], int b[]){
+    public static String findUnion(int[] a, int[] b){
 
-        ArrayList<Integer> union = new ArrayList<>();
-        for ( int i : a){
-            if ( !union.contains(i)) {
-                union.add(i);
-            }
+//        ArrayList<Integer> union = new ArrayList<>();
+        Set<Integer> set = new TreeSet<>();
+        for ( int nums : a){
+            set.add(nums);
         }
-        for ( int element : b ){
-            if ( !union.contains(element)){
-                union.add(element);
-            }
-
+        for (int nums : b){
+            set.add(nums);
         }
-        Collections.sort(union);
 
-        return union;
+            return (Arrays.toString(set.toArray()));
 
     }
 }
